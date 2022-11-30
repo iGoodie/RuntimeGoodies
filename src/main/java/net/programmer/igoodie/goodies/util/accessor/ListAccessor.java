3bea385788173dbe191453e084cae3757fae75ab
Anılcan Metinyurt <igoodie@programmer.net>
2022-11-30 05:24:55 +0300

    feat: make ListAccessor a proxy wrapper

--- src/main/java/net/programmer/igoodie/goodies/util/accessor/ListAccessor.java
+++ src/main/java/net/programmer/igoodie/goodies/util/accessor/ListAccessor.java
@@ -1,8 +1,12 @@
 package net.programmer.igoodie.goodies.util.accessor;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
 
 public class ListAccessor<T> extends IndexAccessor<T> {
 
@@ -26,11 +30,120 @@ public class ListAccessor<T> extends IndexAccessor<T> {
         return list.size();
     }
 
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(Object o) {
+        return false;
+    }
+
     public List<T> subList(int from, int to) {
         if (list.size() == 0) return Collections.emptyList();
         int fromIndex = Math.max(from, 0);
         int toIndex = Math.min(to, list.size() - 1);
         return Collections.unmodifiableList(list.subList(fromIndex, toIndex));
+    }
+
+    public Spliterator<T> spliterator() {
+        return list.spliterator();
+    }
+
+    public Stream<T> stream() {
+        return list.stream();
+    }
+
+    public Stream<T> parallelStream() {
+        return list.parallelStream();
+    }
+
+    @NotNull
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+
+    public void forEach(Consumer<? super T> action) {
+        list.forEach(action);
+    }
+
+    @NotNull
+    public Object @NotNull [] toArray() {
+        return list.toArray();
+    }
+
+    @NotNull
+    public <T1> T1 @NotNull [] toArray(@NotNull T1 @NotNull [] a) {
+        return list.toArray(a);
+    }
+
+    public boolean add(T t) {
+        return list.add(t);
+    }
+
+    public boolean remove(Object o) {
+        return list.remove(o);
+    }
+
+    public boolean containsAll(@NotNull Collection<?> c) {
+        return new HashSet<>(list).containsAll(c);
+    }
+
+    public boolean addAll(@NotNull Collection<? extends T> c) {
+        return list.addAll(c);
+    }
+
+    public boolean addAll(int index, @NotNull Collection<? extends T> c) {
+        return list.addAll(index, c);
+    }
+
+    public boolean removeAll(@NotNull Collection<?> c) {
+        return list.removeAll(c);
+    }
+
+    public boolean removeIf(Predicate<? super T> filter) {
+        return list.removeIf(filter);
+    }
+
+    public boolean retainAll(@NotNull Collection<?> c) {
+        return list.retainAll(c);
+    }
+
+    public void replaceAll(UnaryOperator<T> operator) {
+        list.replaceAll(operator);
+    }
+
+    public void sort(Comparator<? super T> c) {
+        list.sort(c);
+    }
+
+    public void clear() {
+        list.clear();
+    }
+
+    public void add(int index, T element) {
+        list.add(index, element);
+    }
+
+    public T remove(int index) {
+        return list.remove(index);
+    }
+
+    public int indexOf(Object o) {
+        return list.indexOf(o);
+    }
+
+    public int lastIndexOf(Object o) {
+        return list.lastIndexOf(o);
+    }
+
+    @NotNull
+    public ListIterator<T> listIterator() {
+        return list.listIterator();
+    }
+
+    @NotNull
+    public ListIterator<T> listIterator(int index) {
+        return list.listIterator(index);
     }
 
     /* ----------------------------------- */
